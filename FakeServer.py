2c90c1b0971b831651a5bcd8152fe182a5384f46
Colby Leiske <colby.leiske@gmail.com>
2018-01-20 13:43:48 -0800

    Updated server / templates

--- FakeServer.py
+++ FakeServer.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-title = "Parking Counter"
+title = "SlugSpace"
+
+lots = [{'name':"West",'carsInLot':2,'remainingSpots':18},{'name':"East",'carsInLot':5,'remainingSpots':15},{'name':"North",'carsInLot':2,'remainingSpots':18}]
 
 @app.route('/')
-def index():
-    return render_template('index.html', title=title,carsInLot=2,remainingSpots=18,parkingLot="East Remote")
+def parkingLot(): 
+    return render_template('index.html', title=title,lots=lots)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
