796b838b06fae7220c66b2eb79c48f1b89d1a24c
Bradley Bernard <bbernard@paypal.com>
2018-01-20 14:00:34 -0800

    Parking lots query

--- FakeServer.py
+++ FakeServer.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, redirect
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
 
-title = "SlugSpace"
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'slugspace'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-lots = [{'name':"West",'carsInLot':2,'remainingSpots':18},{'name':"East",'carsInLot':5,'remainingSpots':15},{'name':"North",'carsInLot':2,'remainingSpots':18}]
+mysql.init_app(app)
+cursor = mysql.connect().cursor()
 
 @app.route('/')
-def parkingLot(): 
-    return render_template('index.html', title=title,lots=lots)
+def parkingLot():
+    return render_template('index.html', title="SlugSpace", lots=getParkingLots())
+
+def getParkingLots():
+    cursor.execute("SELECT * from PARKING_LOTS")
+    data = cursor.fetchall()
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
