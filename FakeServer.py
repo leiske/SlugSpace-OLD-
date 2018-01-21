0e07f858670290245a2f7f4a5c0b5bd02913f23e
Colby Leiske <colby.leiske@gmail.com>
2018-01-20 16:56:07 -0800

    New format

--- FakeServer.py
+++ FakeServer.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
 from flaskext.mysql import MySQL
+import pusher
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -12,14 +13,34 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 cursor = mysql.connect().cursor()
 
+lots = {"1","2","3"}
+
 @app.route('/')
 def parkingLot():
     return render_template('index.html', title="SlugSpace", lots=getParkingLots())
+
+@app.route('/<lotId>')
+def moreInfo(lotId):
+    if(lotId not in lots):
+        return render_template('404.html')
+    return render_template('moreInfo.html',title="SlugSpace", lot=getParkingLot(lotId), events=getParkingEvents(lotId))
 
 def getParkingLots():
     cursor.execute("SELECT * from PARKING_LOTS")
     data = cursor.fetchall()
     return data
 
+def getParkingLot(lotId):
+    cursor.execute("SELECT * from PARKING_LOTS where id="+lotId)
+    data = cursor.fetchall()
+    return data
+
+
+def getParkingEvents(lotId):
+    cursor.execute("SELECT * from PARKING_EVENTS where lot_id="+lotId+" order by created_at desc")
+    data = cursor.fetchall()
+    return data
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
